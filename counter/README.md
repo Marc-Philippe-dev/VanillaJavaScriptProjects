bdc8dfac5e22b18ea7386c25989b13bf26020285
Marc GNANCADJA <marcgnancadja4@gmail.com>
2021-07-04 20:00:19 +0200

    First commit

---
+++ counter/README.md
@@ -0,0 +1,5 @@
+# Description
+This app will allow us to increase , descrease and reset a counter. 
+We need three button foreach action that we are going to do : increase, decrease and reset. 
+## Fonctionnality
+Whenever the counter is above Zero , the color should be "Green" , when it is below Zero we are gooing to set the color to "Red" and finally when it is equal to Zero , the color will be "Black".
---
+++ counter/app.js
@@ -0,0 +1,34 @@
+// Count Initialisation
+let count = 0;
+
+//Targetting elements
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn)=>{
+    btn.addEventListener("click", (e)=>{
+        console.log(e);
+        const styles = e.currentTarget.classList;
+
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increase")){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent = count;
+
+        if(count < 0){
+            value.style.color = 'red';
+        }
+        if(count > 0){
+            value.style.color = 'green';
+        }
+        if(count === 0){
+            value.style.color = 'black';
+        }
+    });
+});
